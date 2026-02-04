e54b8b83b93ca9c73f714d4d6656ed3f949e20b7
tone.monte <tonelopes.dev@gmail.com>
2026-02-04 10:58:51 -0300

    feat: implement getSales function for retrieving sales data

---
+++ app/_data-access/sale/get-sales.ts
@@ -0,0 +1,53 @@
+import "server-only";
+
+import { db } from "@/app/_lib/prisma";
+
+interface SaleProductDto {
+  productId: string;
+  quantity: number;
+  unitPrice: number;
+  productName: string;
+}
+
+export interface SaleDto {
+  id: string;
+  productNames: string;
+  totalProducts: number;
+  totalAmount: number;
+  date: Date;
+  saleProducts: SaleProductDto[];
+}
+
+export const getSales = async (): Promise<SaleDto[]> => {
+  const sales = await db.sale.findMany({
+    include: {
+      saleProducts: {
+        include: { product: true },
+      },
+    },
+  });
+  return sales.map((sale) => ({
+    id: sale.id,
+    date: sale.date,
+    productNames: sale.saleProducts
+      .map((saleProduct) => saleProduct.product.name)
+      .join(" • "),
+    totalAmount: sale.saleProducts.reduce(
+      (acc, saleProduct) =>
+        acc + saleProduct.quantity * Number(saleProduct.unitPrice),
+      0,
+    ),
+    totalProducts: sale.saleProducts.reduce(
+      (acc, saleProduct) => acc + saleProduct.quantity,
+      0,
+    ),
+    saleProducts: sale.saleProducts.map(
+      (saleProduct): SaleProductDto => ({
+        productId: saleProduct.productId,
+        productName: saleProduct.product.name,
+        quantity: saleProduct.quantity,
+        unitPrice: Number(saleProduct.unitPrice),
+      }),
+    ),
+  }));
+};
