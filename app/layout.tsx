fdf5985ca2be164348d9573f7f55e3743c8bca90
tone.monte <tonelopes.dev@gmail.com>
2026-02-04 13:01:10 -0300

    feat: Enhance RootLayout to include HTML structure and font styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,25 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Inter } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Stockly",
   description: "Sistema de gerenciamento de estoque",
 };
 
+const inter = Inter({
+  subsets: ["latin"],
+  display: "auto",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return children;
+  return (
+    <html lang="pt-BR">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
 }
-
