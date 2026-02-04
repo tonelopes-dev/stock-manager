3ce60513152494cece8eac972f948fe5944240d8
tone.monte <tonelopes.dev@gmail.com>
2026-02-04 13:01:19 -0300

    feat: Add ProductsLayout component for product management layout

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -15,7 +15,7 @@ export const dynamic = "force-static";
 const ProductsPage = async () => {
   const products = await getProducts();
   return (
-    <div className="m-8 w-full space-y-8 overflow-auto rounded-lg bg-white p-8">
+    <div className="m-8 space-y-8 overflow-auto rounded-lg bg-white p-8">
       <Header>
         <HeaderLeft>
           <HeaderSubtitle>Gestão de Produtos</HeaderSubtitle>
