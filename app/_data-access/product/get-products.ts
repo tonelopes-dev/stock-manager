67539b094a51d00daac71e171c91f2550bd34812
tone.monte <tonelopes.dev@gmail.com>
2026-02-04 11:00:21 -0300

    feat: enhance getProducts function to include price and stock status

--- app/_data-access/product/get-products.ts
+++ app/_data-access/product/get-products.ts
@@ -1,6 +1,20 @@
 import "server-only";
-import { db, Product } from "@/app/_lib/prisma";
 
-export const getProducts = async (): Promise<Product[]> => {
-  return await db.product.findMany();
+import { db } from "@/app/_lib/prisma";
+import { Product } from "@prisma/client";
+
+export type ProductStatusDto = "IN_STOCK" | "OUT_OF_STOCK";
+
+export interface ProductDto extends Omit<Product, "price"> {
+  price: number;
+  status: ProductStatusDto;
+}
+
+export const getProducts = async (): Promise<ProductDto[]> => {
+  const products = await db.product.findMany({});
+  return products.map((product) => ({
+    ...product,
+    price: Number(product.price),
+    status: product.stock > 0 ? "IN_STOCK" : "OUT_OF_STOCK",
+  }));
 };
