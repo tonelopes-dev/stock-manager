b26f7e4db7ccb8e9927bf1b704aad907ff89fa3e
tone.monte <tonelopes.dev@gmail.com>
2026-02-04 13:01:47 -0300

    feat: Add SalesLayout component for sales page layout and adjust SalesPage styling

--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -28,7 +28,7 @@ const SalesPage = async () => {
     productOptions,
   }));
   return (
-    <div className="m-8 w-full space-y-8 overflow-auto rounded-lg bg-white p-8">
+    <div className="m-8 space-y-8 overflow-auto rounded-lg bg-white p-8">
       <Header>
         <HeaderLeft>
           <HeaderSubtitle>Gestão de Vendas</HeaderSubtitle>
