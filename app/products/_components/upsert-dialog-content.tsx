daa76c1fd95d124448b9a96a2c337946a0d295ae
tone.monte <tonelopes.dev@gmail.com>
2026-02-04 10:57:59 -0300

    feat: enhance UpsertProductDialogContent with improved form handling and UI updates

--- app/products/_components/upsert-dialog-content.tsx
+++ app/products/_components/upsert-dialog-content.tsx
@@ -1,161 +1,154 @@
 "use client";
-import { useEffect } from "react";
+
+import { upsertProduct } from "@/app/_actions/product/upsert-product";
+import {
+  upsertProductSchema,
+  UpsertProductSchema,
+} from "@/app/_actions/product/upsert-product/schema";
 import { Button } from "@/app/_components/ui/button";
-import { DialogHeader } from "@/app/_components/ui/dialog";
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/app/_components/ui/dialog";
 import {
   FormField,
   FormItem,
   FormLabel,
   FormControl,
   FormMessage,
+  Form,
 } from "@/app/_components/ui/form";
-import {
-  DialogContent,
-  DialogTitle,
-  DialogDescription,
-  DialogClose,
-} from "@/app/_components/ui/dialog";
-import { Loader2 } from "lucide-react";
 import { Input } from "@/app/_components/ui/input";
-import { NumericFormat } from "react-number-format";
-import { Form } from "@/app/_components/ui/form";
-import {
-  UpsertProductSchema,
-  upsertProduct,
-} from "@/app/_actions/product/upsert-product";
-import { toast } from "sonner";
-import { upsertProductSchema } from "@/app/_actions/product/upsert-product/schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2Icon } from "lucide-react";
+import { useAction } from "next-safe-action/hooks";
+import { Dispatch, SetStateAction } from "react";
 import { useForm } from "react-hook-form";
+import { NumericFormat } from "react-number-format";
+import { toast } from "sonner";
 
 interface UpsertProductDialogContentProps {
   defaultValues?: UpsertProductSchema;
-  onSucess?: () => void;
+  setDialogIsOpen: Dispatch<SetStateAction<boolean>>;
 }
 
 const UpsertProductDialogContent = ({
   defaultValues,
-  onSucess,
+  setDialogIsOpen,
 }: UpsertProductDialogContentProps) => {
+  const { execute: executeUpsertProduct } = useAction(upsertProduct, {
+    onSuccess: () => {
+      toast.success("Produto salvo com sucesso.");
+      setDialogIsOpen(false);
+    },
+    onError: () => {
+      toast.error("Ocorreu um erro ao salvar o produto.");
+    },
+  });
   const form = useForm<UpsertProductSchema>({
+    shouldUnregister: true,
     resolver: zodResolver(upsertProductSchema),
     defaultValues: defaultValues ?? {
+      id: "",
       name: "",
       price: 0,
       stock: 1,
     },
   });
 
-  useEffect(() => {
-    if (!defaultValues) {
-      form.reset();
-    } else {
-      form.reset(defaultValues);
-    }
-  }, [defaultValues, form]);
-  const isEditing = !!defaultValues;
-  const onSubmit = async (data: UpsertProductSchema) => {
-    try {
-      await upsertProduct({ ...data, id: defaultValues?.id });
-      onSucess?.();
-      toast.success("Produto cadastrado com sucesso!");
-    } catch (e) {
-      console.log(e);
-      toast.error("Erro ao cadastrar produto!");
-    }
+  const onSubmit = (data: UpsertProductSchema) => {
+    executeUpsertProduct({ ...data, id: defaultValues?.id });
   };
+
+  const isEditing = !!defaultValues;
+
   return (
     <DialogContent>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <DialogHeader>
-            <DialogTitle>{isEditing ? "Editar" : "Novo"} Produto</DialogTitle>
-            <DialogDescription>
-              <div className="grid grid-cols-2 gap-4">
-                <div className="col-span-2">
-                  <FormField
-                    control={form.control}
-                    name="name"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Nome</FormLabel>
-                        <FormControl>
-                          <Input {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  {form.formState.errors.name && (
-                    <p className="mt-1 text-sm text-red-600">
-                      {form.formState.errors.name.message}
-                    </p>
-                  )}
-                </div>
-                <div className="col-span-1">
-                  <FormField
-                    control={form.control}
-                    name="stock"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Estoque</FormLabel>
-                        <FormControl>
-                          <Input type="number" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-                <div className="col-span-1">
-                  <FormField
-                    control={form.control}
-                    name="price"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Valor</FormLabel>
-                        <FormControl>
-                          <NumericFormat
-                            thousandSeparator="."
-                            decimalSeparator=","
-                            fixedDecimalScale
-                            decimalScale={2}
-                            {...field}
-                            prefix="R$ "
-                            allowNegative={false}
-                            customInput={Input}
-                            onValueChange={(values) =>
-                              field.onChange(values.floatValue)
-                            }
-                            onChange={() => {}}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-              </div>
-              <div className="mt-4 flex justify-end gap-4">
-                <DialogClose asChild>
-                  <Button variant="secondary" type="reset">
-                    Cancelar
-                  </Button>
-                </DialogClose>
-
-                <Button
-                  type="submit"
-                  disabled={form.formState.isSubmitting}
-                  className="gap-1.5"
-                >
-                  {form.formState.isSubmitting && (
-                    <Loader2 size={16} className="mr-2 animate-spin" />
-                  )}
-                  Salvar
-                </Button>
-              </div>
-            </DialogDescription>
+            <DialogTitle>{isEditing ? "Editar" : "Criar"} produto</DialogTitle>
+            <DialogDescription>Insira as informações abaixo</DialogDescription>
           </DialogHeader>
+
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nome</FormLabel>
+                <FormControl>
+                  <Input placeholder="Digite o nome do produto" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="price"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Preço</FormLabel>
+                <FormControl>
+                  <NumericFormat
+                    thousandSeparator="."
+                    decimalSeparator=","
+                    fixedDecimalScale
+                    decimalScale={2}
+                    prefix="R$ "
+                    allowNegative={false}
+                    customInput={Input}
+                    onValueChange={(values) =>
+                      field.onChange(values.floatValue)
+                    }
+                    {...field}
+                    onChange={() => {}}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="stock"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Estoque</FormLabel>
+                <FormControl>
+                  <Input
+                    type="number"
+                    placeholder="Digite o estoque do produto"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="secondary" type="reset">
+                Cancelar
+              </Button>
+            </DialogClose>
+            <Button
+              type="submit"
+              disabled={form.formState.isSubmitting}
+              className="gap-1.5"
+            >
+              {form.formState.isSubmitting && (
+                <Loader2Icon className="animate-spin" size={16} />
+              )}
+              Salvar
+            </Button>
+          </DialogFooter>
         </form>
       </Form>
     </DialogContent>
