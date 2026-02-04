68e6c0b9d860780d385a3c9f6ff5c00320a60e9d
tone.monte <tonelopes.dev@gmail.com>
2026-02-04 10:57:51 -0300

    feat: implement SalesPage component for managing sales and displaying data

--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -1,5 +1,49 @@
-const PageSales = () => {
-  return <h1>Page Sales</h1>;
+import Header, {
+  HeaderLeft,
+  HeaderRight,
+  HeaderSubtitle,
+  HeaderTitle,
+} from "../_components/header";
+import { ComboboxOption } from "../_components/ui/combobox";
+import { DataTable } from "../_components/ui/data-table";
+import { getProducts } from "../_data-access/product/get-products";
+import { getSales } from "../_data-access/sale/get-sales";
+import UpsertSaleButton from "./_components/create-sale-button";
+import { saleTableColumns } from "./_components/table-columns";
+
+// Essa página será montada uma vez e reutilizada (SSG), podendo ser incrementada de forma regenerativa (ISR)
+export const dynamic = "force-static";
+export const revalidate = 10;
+
+const SalesPage = async () => {
+  const sales = await getSales();
+  const products = await getProducts();
+  const productOptions: ComboboxOption[] = products.map((product) => ({
+    label: product.name,
+    value: product.id,
+  }));
+  const tableData = sales.map((sale) => ({
+    ...sale,
+    products,
+    productOptions,
+  }));
+  return (
+    <div className="m-8 w-full space-y-8 overflow-auto rounded-lg bg-white p-8">
+      <Header>
+        <HeaderLeft>
+          <HeaderSubtitle>Gestão de Vendas</HeaderSubtitle>
+          <HeaderTitle>Vendas</HeaderTitle>
+        </HeaderLeft>
+        <HeaderRight>
+          <UpsertSaleButton
+            products={products}
+            productOptions={productOptions}
+          />
+        </HeaderRight>
+      </Header>
+      <DataTable columns={saleTableColumns} data={tableData} />
+    </div>
+  );
 };
 
-export default PageSales;
+export default SalesPage;
